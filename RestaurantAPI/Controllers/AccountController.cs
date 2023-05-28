777585ebde797a8b956a2aca6dae694ef5412c98
Jarosław Świderski <jarekswiderski2@gmail.com>
2023-05-28 17:30:13 +0200

    2

---
+++ RestaurantAPI/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Models;
+using RestaurantAPI.Services;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController:ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+
+        [HttpPost("login")]
+        public ActionResult Login(LoginDto dto)
+        {
+            string token = _accountService.GenerateJwt(dto);
+
+            return Ok(token);
+        }
+
+        [HttpPost("register")]
+        public ActionResult RegisterUser([FromBody] RegisterUserDto dto)
+        {
+            _accountService.RegisterUser(dto);
+
+            return Ok();
+        }
+    }
+}
